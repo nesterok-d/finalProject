1c9d2705d59fb1250f9b928413f490bf31779986
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-26 13:59:30 +0700

    added commentaries

--- peges/main_page.py
+++ peges/main_page.py
@@ -1,10 +1,8 @@
 #наследование свойств базовой страницы из класса BasePage
 from .base_page import BasePage
 
-#наследование от MainPageLocators
-from .locators import MainPageLocators
-
 #создание класса "основная станица"
 class MainPage(BasePage):
+    # функция инициализации страницы
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
