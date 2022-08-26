13e372c06682444a97bc8622de7fbdf91bd11a5c
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-26 14:07:34 +0700

    added commentaries

--- peges/basket_page.py
+++ peges/basket_page.py
@@ -2,13 +2,15 @@
 from .base_page import BasePage
 from .locators import BasketPageLocators
 
-
+# создание класса для методов страницы корзины
 class BasketPage(BasePage):
 
+    # метод проверки, что в корзине нет элементов
     def should_be_no_items_in_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.ITEMS_IN_BASKET), \
             "items in basket is presented, but should not be"
 
+    # метод проверки, что в корзине есть сообщение "корзина пуста"
     def should_be_message_in_basket(self):
         assert self.is_element_present(*BasketPageLocators.MESSAGE_IN_BASKET), \
             "message in basket is not presented, but should be"
