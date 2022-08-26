039c013a05dce5dc28dfef08393b443f37460171
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-26 12:51:21 +0700

    added commentaries

--- test_product_page.py
+++ test_product_page.py
@@ -9,6 +9,8 @@ from peges.product_page import ProductPage
 
 
 # создаем тестовые сцениарии
+
+#проверка корректности сообщений о добавлении в корзину
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -31,7 +33,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_success_message()
     page.compare_titles_of_books()
 
-
+#негативный тест: пользователь не видит сообщения о добавлении товара после добавления
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -44,7 +46,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
     page.compare_titles_of_books()
 
-
+#пользователь не видит сообщения о добавлении товара на странице товара до добавления в корзину
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
@@ -52,7 +54,7 @@ def test_guest_cant_see_success_message(browser):
     time.sleep(1)
     page.should_not_be_success_message()
 
-
+#Негативный тест: сообщение о добавлении в корзину исчезает
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -64,67 +66,70 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_is_disappeared_success_message()
 
-
+#группа тестов для гостя
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
+
+    # на странице продукта для гостя отображается кнопка для перехода на страницу логина
     def test_guest_should_see_login_link_on_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_login_link()
 
+    # гость может перейти на страницу логина со станицы продукта
     @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser,
-                           link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-        login_page = LoginPage(browser, browser.current_url)  # открываем страницу с формами логина и регистрации
-        login_page.should_be_login_page()  # выполняем проверку на отображение страницы
+        page = ProductPage(browser,  link)                     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                                            # открываем страницу
+        page.go_to_login_page()                                # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)   # открываем страницу с формами логина и регистрации
+        login_page.should_be_login_page()                      # выполняем проверку на отображение страницы
 
+    # гость не видит товары в пустой корзине, показано сообщение "корзина пуста"
     @pytest.mark.need_review
     def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser,
-                           link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()
-        page.is_element_present(*BasePageLocators.BASKET_LINK)
-        page.go_to_basket_page()
-        basket = BasketPage(browser, browser.current_url)
-        basket.should_be_message_in_basket()
-        basket.should_be_no_items_in_basket()
+        page = ProductPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                                            # открываем страницу
+        page.is_element_present(*BasePageLocators.BASKET_LINK) # проверяем наличие кнопки для перехода в корзину
+        page.go_to_basket_page()                               # выполняем метод страницы — переходим на страницу корзины
+        basket = BasketPage(browser, browser.current_url)      # открываем страницу корзины
+        basket.should_be_message_in_basket()                   # выполняем проверку на поиск сообщения "корзина пуста"
+        basket.should_be_no_items_in_basket()                  # выполняем проверку на отображение элементов списка на странице
 
-
+#группа тестов для авторизованного пользователя
 @pytest.mark.login
 class TestUserAddToBasketFromProductPage():
+    #фикстура для авторизвации пользователя на каждом тесте
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        page = ProductPage(browser,
-                           "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/")  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()
-        page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
-        email = str(time.time()) + "@fakemail.org"
-        password = str(time.time())
-        login_page.register_new_user(email, password)
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                                            # открываем страницу
+        page.go_to_login_page()                                # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)   # открываем страницу с формами логина и регистрации
+        email = str(time.time()) + "@fakemail.org"             # создаем тестовый email
+        password = str(time.time())                            # создаем тестовый пароль
+        login_page.register_new_user(email, password)          # вызываем функцию для регистарции пользователя
 
+    #негативный тест: авторизавнный пользователь не видит сообщения о добавлении товара в корзину
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser,
-                           link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()
-        page.is_element_present(*BasePageLocators.BASKET_LINK)
-        page.go_to_basket_page()
-        basket = BasketPage(browser, browser.current_url)
-        basket.should_be_message_in_basket()
-        basket.should_be_no_items_in_basket()
+        page = ProductPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                                            # открываем страницу
+        page.is_element_present(*BasePageLocators.BASKET_LINK) # проверяем наличие кнопки для перехода в корзину
+        page.go_to_basket_page()                               # выполняем метод страницы — переходим на страницу корзины
+        basket = BasketPage(browser, browser.current_url)      # открываем страницу корзины
+        basket.should_be_message_in_basket()                   # выполняем проверку на поиск сообщения "корзина пуста"
+        basket.should_be_no_items_in_basket()                  # выполняем проверку на отображение элементов списка на странице
 
+    # авторизованный пользователь может добавить товар в корзину
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser,
-                           link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page = ProductPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()
         page.is_element_present(*BasePageLocators.BASKET_LINK)
         page.go_to_basket_page()
