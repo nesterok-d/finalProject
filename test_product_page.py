23a5efe57b5573c81b108a06c0a7ae31393e7587
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-26 11:14:21 +0700

    added product page tests

---
+++ test_product_page.py
@@ -0,0 +1,119 @@
+import time
+import pytest
+
+#Наследуем методы классов
+from peges.basket_page import BasketPage
+from peges.locators import BasePageLocators
+from peges.login_page import LoginPage
+from peges.product_page import ProductPage
+
+#создаем тестовые сцениарии
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.should_be_btn_add_to_basket()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_success_message()
+    assert page.take_success_message() == page.take_title_the_book()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.should_be_btn_add_to_basket()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.should_be_btn_add_to_basket()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_is_disappeared_success_message()
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)                      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                                            # открываем страницу
+        page.go_to_login_page()                                # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)   # открываем страницу с формами логина и регистрации
+        login_page.should_be_login_page()                      # выполняем проверку на отображение страницы
+
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.is_element_present(*BasePageLocators.BASKET_LINK)
+        page.go_to_basket_page()
+        basket = BasketPage(browser, browser.current_url)
+        basket.should_be_message_in_basket()
+        basket.should_be_no_items_in_basket()
+
+@pytest.mark.login
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/")  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        login_page.register_new_user(email, password)
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.is_element_present(*BasePageLocators.BASKET_LINK)
+        page.go_to_basket_page()
+        basket = BasketPage(browser, browser.current_url)
+        basket.should_be_message_in_basket()
+        basket.should_be_no_items_in_basket()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()
+        page.is_element_present(*BasePageLocators.BASKET_LINK)
+        page.go_to_basket_page()
+        basket = BasketPage(browser, browser.current_url)
+        basket.should_be_message_in_basket()
+        basket.should_be_no_items_in_basket()
